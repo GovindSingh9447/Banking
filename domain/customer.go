6377ed17e86c544f0030925ce7dd07016d6698c2
GovindSingh9447 <govindkrsingh0605@gmail.com>
2024-04-06 11:22:34 +0530

    Data Transfer Object

--- domain/customer.go
+++ domain/customer.go
@@ -1,22 +1,45 @@
 package domain
 
-import "Banking/errors"
+import (
+	"Banking/dto"
+	"Banking/errors"
+)
 
-type Customer struct{
-
-
-	Id   string `db:"customer_id"`
-	Name string
-	City  string
-	Zipcode string
+type Customer struct {
+	Id          string `db:"customer_id"`
+	Name        string
+	City        string
+	Zipcode     string
 	DateofBirth string `db:"date_of_birth"`
-	Status   string 
+	Status      string
 }
 
+func (c Customer) statusAsText() string {
+	statusAsText := "active"
+	if c.Status == "0"{
+		statusAsText = "inactive"
+	}
+	return statusAsText
 
-type CustomerRepository interface{
+}
+
+func (c Customer) ToDto() dto.CustomerResponse {
+
+	
+    return dto.CustomerResponse{
+
+	Id: c.Id, 
+	Name: c.Name,
+	City: c.City,
+	Zipcode: c.Zipcode,
+	DateofBirth: c.DateofBirth,
+	Status: c.statusAsText(),
+}
+
+}
+
+type CustomerRepository interface {
 	FindAll(status string) ([]Customer, *errors.AppError)
 
-	ById(string) (*Customer,*errors.AppError)
+	ById(string) (*Customer, *errors.AppError)
 }
-
--- service/customerService.go
+++ service/customerService.go
@@ -1,35 +1,43 @@
 package service
 
-import "Banking/domain"
-import "Banking/errors"
+import (
+	"Banking/domain"
+	"Banking/dto"
+	"Banking/errors"
+)
 
-
+// primary port
 type CustomerService interface {
 	GetAllCustomer(string) ([]domain.Customer, *errors.AppError)
-	GetCustomer(string) (*domain.Customer, *errors.AppError)
+	GetCustomer(string) (*dto.CustomerResponse, *errors.AppError)
 }
 
-
-type DefaultCustomerService struct{
+type DefaultCustomerService struct {
 	repo domain.CustomerRepository
 }
 
-func (s DefaultCustomerService) GetAllCustomer(status string) ([]domain.Customer,*errors.AppError){
+func (s DefaultCustomerService) GetAllCustomer(status string) ([]domain.Customer, *errors.AppError) {
 	if status == "active" {
 		status = "1"
-	}else if status == "inactive" {
+	} else if status == "inactive" {
 		status = "0"
-	}else{
+	} else {
 		status = ""
 	}
 	return s.repo.FindAll(status)
 }
 
-func (s DefaultCustomerService) GetCustomer(id string) (*domain.Customer,*errors.AppError){
-	return s.repo.ById(id)
+func (s DefaultCustomerService) GetCustomer(id string) (*dto.CustomerResponse, *errors.AppError) {
+	c, err := s.repo.ById(id)
+	if err != nil {
+		return nil, err
+	}
+
+	c.ToDto()
+	respone := c.ToDto()
+	return &respone, nil
 }
 
-
-func NewCustomerService (repository domain.CustomerRepository) DefaultCustomerService {
+func NewCustomerService(repository domain.CustomerRepository) DefaultCustomerService {
 	return DefaultCustomerService{repository}
 }
